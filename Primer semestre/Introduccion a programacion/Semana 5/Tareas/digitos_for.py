f45a518952662fe55850294a84083dd3317aa423
Camilo <mothbms@gmail.com>
2025-04-08 13:04:16 -0400

    fix digitos

--- Primer semestre/Introduccion a programacion/Semana 5/Tareas/digitos_for.py
+++ Primer semestre/Introduccion a programacion/Semana 5/Tareas/digitos_for.py
@@ -1,11 +1,9 @@
-estado = 1
-for a in range(0, 2, estado):
+i = True
+while i == True:
     contador = 0
     num = int(input("inserte el numero para contar los digitos: "))
     num = str(num)
     for i in num:
         contador = contador + 1
     print("la cantidad de digitos del numero son: ", contador)
-    estado = int(input("quiere seguir calculando?\n 1 = continuar\n 0 = salir\n : "))
-    if (estado == 1):
-        a = a - 1
+    i = int(input("quiere seguir calculando?\n 1 = seguir\n 0 = salir\n : "))
