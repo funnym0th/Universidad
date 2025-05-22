f8551a7166932f04b4a7e93c9dffe3754b1c4ffc
Camilo <52089905+funnym0th@users.noreply.github.com>
2025-05-22 18:20:08 -0400

    Add files via upload

---
+++ xd.py
@@ -0,0 +1,11 @@
+# Elegir cabaña y mostrar ocupacion
+# Reservar espacios o dias en la cabaña
+# Mostrar valor a pagar * dias 
+cabanas = [[1, 2, 3], [10000, 20000, 30000], [0, 0, 0]]
+print("Lista de cabañas: ")
+for i in cabanas[0]:
+    print(f"Cabaña {i}: Para {cabanas[0][i - 1]} persona(s), valor ${cabanas[1][i - 1]}")
+eleccion = int(input("Que cabaña desea reservar?\n : "))
+while eleccion in cabanas[0] and cabanas[2][eleccion - 1] < cabanas[0][eleccion - 1]:
+    print(f"La ocupacion de la cabaña {eleccion} es de {cabanas[2][eleccion - 1]} persona(s)")
+    
