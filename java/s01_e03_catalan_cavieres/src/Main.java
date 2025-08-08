f4389587f1ed99cae8f07b5f58298fc1be5ce0ba
Camilo <52089905+funnym0th@users.noreply.github.com>
2025-08-08 09:53:49 -0400

    Create Main.java

---
+++ java/s01_e03_catalan_cavieres/src/Main.java
@@ -0,0 +1,8 @@
+package s01_e03_catalan_cavieres;
+
+public class Main {
+    public static void main(String[] args){
+        CuentaBancaria persona1 = new CuentaBancaria(5000f, "Camilo Catalán", "22.170.322-7");
+        persona1.mostrarSaldo();
+    }
+}
